269df3c9d6ff203e37451ae6e5304ad565999f28
Rouseywars <72237500+Rouseywars@users.noreply.github.com>
2020-12-25 14:22:17 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5._1
+{
+    static class Program //Средний уровень
+    {
+        static void Main(string[] args)
+        {
+            string S = new String(new char[] { 'Р', 'о', 'с', 'с', 'и', 'я' });
+            string s = string.Join(" ", S.ToCharArray());
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
